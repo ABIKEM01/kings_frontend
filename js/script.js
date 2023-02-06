aabdeb7c0c681f61e7e8ff1f46d45245d083a22a
abikem01 <babayod@yahoo.com>
2023-02-06 10:58:59 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// navbar toggling
+const navbarShowBtn = document.querySelector(".navbar-show-btn");
+const navbarCollapseDiv = document.querySelector(".navbar-collapse");
+const navbarHideBtn = document.querySelector(".navbar-hide-btn");
+
+navbarShowBtn.addEventListener("click", function () {
+  navbarCollapseDiv.classList.add("navbar-show");
+});
+navbarHideBtn.addEventListener("click", function () {
+  navbarCollapseDiv.classList.remove("navbar-show");
+});
+
+// changing search icon image on window resize
+window.addEventListener("resize", changeSearchIcon);
+function changeSearchIcon() {
+  let winSize = window.matchMedia("(min-width: 1200px)");
+  if (winSize.matches) {
+    document.querySelector(".search-icon img").src = "images/search-icon.png";
+  } else {
+    document.querySelector(".search-icon img").src =
+      "images/search-icon-dark.png";
+  }
+}
+changeSearchIcon();
+
+// stopping all animation and transition
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
+
+let contactform = document.getElementById("contactform");
+
+contactform.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let name = document.getElementById("name");
+  let email = document.getElementById("email");
+  let message = document.getElementById("message");
+
+  if (name.value == "" || email.value == "" || message.value == "") {
+    alert("Ensure you input a value in both fields!");
+  } else {
+    // perform operation with form input
+    alert("This form has been successfully submitted!");
+    console.log(
+      `This form has a name of ${name.value} and email of ${email.value} message of  ${message.value}`
+    );
+
+    name.value = "";
+    email.value = "";
+    message.value = "";
+  }
+});
